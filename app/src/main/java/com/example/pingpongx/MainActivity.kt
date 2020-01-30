aa57247efd9d24f2f699f43247b02796133200a3
Logon Aluno <logonaluno@local.com>
2020-01-29 22:35:34 -0300

    Salvando Implementação

--- app/src/main/java/com/example/pingpongx/MainActivity.kt
+++ app/src/main/java/com/example/pingpongx/MainActivity.kt
@@ -2,15 +2,32 @@ package com.example.pingpongx
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
+    // Inicializar só quando for usada.
+    lateinit var mainViewModel: MainViewModel
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainViewModel = ViewModelProvider(this)
+            .get(mainViewModel::class.java)
+
         setUpPlayers()
+
+        tvPlayerOneScore.text = mainViewModel.playerOneScore.toString()
+        
+        btPlayerOneScore.setOnClickListener {
+            tvPlayerOneScore.text = mainViewModel.playerOneScore.toString()
+        }
+
+
     }
 
     private fun setUpPlayers() {
